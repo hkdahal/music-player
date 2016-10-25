668007a3236347ad13910fb1e13204e60cf9d578
Hari Dahal <hxd1478@rit.edu>
2016-10-24 21:10:30 -0400

    Removed 'nice scroll' and updated the featured_artists

--- hPlay/static/js/scripts.js
+++ hPlay/static/js/scripts.js
@@ -1,19 +1,6 @@
 (function() {
     "use strict";
 
-    // custom scrollbar
-
-    $("html").niceScroll({styler:"fb",cursorcolor:"#45B39D", cursorwidth: '5', cursorborderradius: '10px', background: '#444', spacebarenabled:false, cursorborder: '0',  zindex: '1000'});
-
-    $(".left-side").niceScroll({styler:"fb",cursorcolor:"#45B39D", cursorwidth: '3', cursorborderradius: '10px', background: '#444', spacebarenabled:false, cursorborder: '0'});
-
-
-    $(".left-side").getNiceScroll();
-    if ($('body').hasClass('left-side-collapsed')) {
-        $(".left-side").getNiceScroll().hide();
-    }
-
-
 
     // Toggle Left Menu
    jQuery('.menu-list > a').click(function() {
